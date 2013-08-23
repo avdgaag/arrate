2522c0ffd81cfb3393a0a6059e5dfc55de5b4a9f
Arjan van der Gaag <arjan@kabisa.nl>
2013-08-23 15:37:47 +0200

    Specify default generator preferences

--- lib/arrate.rb
+++ lib/arrate.rb
@@ -6,6 +6,15 @@ module Arrate
     class Railtie < ::Rails::Railtie
       config.app_generators do |g|
         g.templates.unshift File.expand_path('../templates', __FILE__)
+        g.template_engine     :haml
+        g.test_framework      :rspec, fixture: true, fixture_replacement: :factory_girl
+        g.view_specs          false
+        g.helper_specs        false
+        g.helper              false
+        g.javascripts         false
+        g.stylesheets         false
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
+        g.assets              false
       end
     end
   end
